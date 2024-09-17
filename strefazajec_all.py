919e0c06a96d5ef2e8dd99d8b41213ceed54994c
MarcinVaadin <marcin@vaadin.com>
2024-09-17 12:22:50 +0200

    wiele kalendarzy

--- strefazajec_all.py
+++ strefazajec_all.py
@@ -5,18 +5,24 @@ from icalendar import Alarm, Calendar, Event
 import requests
 import dateparser
 
-classes = {
-    'Minecraft' : 'https://www.strefazajec.pl/course/view/id/65496',
-    'Szachy': 'https://www.strefazajec.pl/course/view/id/65821',
-    'Szycie': 'https://www.strefazajec.pl/course/view/id/64967',
-    'Pianino': 'https://www.strefazajec.pl/course/view/id/63775'
-}
+base_url = 'https://www.strefazajec.pl'
+company_url = base_url + '/company/DK-Stare-Babice-id864.html'
 
 def load(url):
     return requests.get(url).text
 
 def strip(text):
     return re.sub(r'\s+', ' ', text.strip())
+
+def find_courses(content):
+    list = []
+    soup = BeautifulSoup(content, 'html.parser')
+    course_list = soup.select_one('div#course_list')
+    courses = course_list.select('div.course-record')
+    for course in courses:
+        href = course.select_one('a').attrs['href']
+        list.append(href)
+    return list
 
 def to_datetime(text):
     return dateparser.parse(text, languages=['pl'])
@@ -28,7 +34,7 @@ def to_timedelta(text):
     m = int(m[0]) if len(m) > 0 else 0
     return timedelta(hours=h, minutes=m)
 
-def to_event(title_short, title_full, lesson):
+def to_event(title, lesson):
     tds = lesson.find_all("td")
     date = strip(tds[0].text)
     duration = strip(tds[1].text)
@@ -37,8 +43,7 @@ def to_event(title_short, title_full, lesson):
     dtstart = to_datetime(date)
     dtend = dtstart + to_timedelta(duration)
     event = Event()
-    event.add('summary', title_short)
-    event.add('description', title_full)
+    event.add('summary', title)
     event.add('dtstart', dtstart)
     event.add('dtend', dtend)
     alarm = Alarm()
@@ -47,20 +52,38 @@ def to_event(title_short, title_full, lesson):
     event.add_component(alarm)
     return event
 
-cal = Calendar()
-cal.add('prodid', '-//Kalendarz Szkolny//example.com//')
-cal.add('version', '2.0')
+def parse_to_ics(url, filename):
+    cal = Calendar()
+    cal.add('prodid', '-//Kalendarz Szkolny//example.com//')
+    cal.add('version', '2.0')
 
-for key in classes:
-    content = load(classes[key])
-    
+    content = load(url)    
     soup = BeautifulSoup(content, 'html.parser')
     title = soup.select_one("h1[class!=logo]").text
 
     for lesson in soup.find_all("tr", {"class":"lesson_record"}):
-        event = to_event(key, title, lesson)
+        event = to_event(title, lesson)
         cal.add_component(event)
 
-f = open("strefazajec.ics", "wb")
-f.write(cal.to_ical())
-f.close()
+    f = open(filename, "wb")
+    f.write(cal.to_ical())
+    f.close()
+    print('DONE: ' + title)
+
+
+content = load(company_url)
+soup = BeautifulSoup(content, 'html.parser')
+paginator = soup.select_one("div[class=paginator]")
+
+list = []
+
+pages = re.findall(r'\d+', paginator.text)
+for page in pages:
+    if page == '1':
+        list += find_courses(content)
+    else:
+        list += find_courses(load(company_url + '?page=' + page))
+
+for href in list:
+    id = re.findall(r'\d+', href)
+    parse_to_ics(base_url + href, 'strefazajec/' + id[0] + '.ics')
