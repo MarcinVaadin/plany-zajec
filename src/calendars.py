a2feccdd081b09b9b414ba647271706b0abe9f2f
MarcinVaadin <marcin@vaadin.com>
2024-09-19 00:07:35 +0200

    as list

--- src/calendars.py
+++ src/calendars.py
@@ -15,15 +15,15 @@ services = load_services()
 
 with open("KALENDARZE.md", 'w') as f:
     # strefazajec.pl
-    f.write("# Strefa Zajęć\n")
+    f.write("# Strefa Zajęć\n\n")
     for organization in services['strefazajec']:
         metadata = strefazajec(organization, 'calendars/strefazajec')
-        f.write("## " + metadata['name'] + "\n")
+        f.write("## " + metadata['name'] + "\n\n")
         for course in metadata['courses']:
-            f.write(link(course['title'], course['ics']))
+            f.write('- ' + link(course['title'], course['ics']))
 
     # tygodniowe menu
-    f.write("# Tygodniowe menu szkoły w Zielonkach-Parceli\n")
+    f.write("# Tygodniowe menu szkoły w Zielonkach-Parceli\n\n")
     metadata = tygodniowemenu('calendars/tygodniowemenu')
     for clazz in metadata:
-        f.write(link(clazz['name'], clazz['ics']))
+        f.write('- ' + link(clazz['name'], clazz['ics']))
